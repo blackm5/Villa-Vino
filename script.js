10db85bb6f815f81e14eed163239a5a8757a97cc
blackm5 <125949105+blackm5@users.noreply.github.com>
2023-08-08 02:43:28 -0300

    script

---
+++ script.js
@@ -0,0 +1,55 @@
+/*alert('Seja bem-vindo ao Villa Vino!')*/
+function Brie() {
+    var Brie = window.document.querySelector('div#Brie')
+    Brie.innerHTML = `<p class="infor">&#x02022;Queijo Brie;<br>&#x02022;Massa folhada e <br>&#x02022;Geleia Artesanal de Morango.</p>`
+}
+function Burrata() {
+    var Burrata = window.document.querySelector('div#Burrata')
+    Burrata.innerHTML = `<p class="infor">&#x02022;Burrata;<br>&#x02022;Molho Artesanal Tomate Cereja Confitado;<br>&#x02022;Azeite Trufado e<br>&#x02022;Especiarias.</p>`
+}
+function Caprese() {
+    var Burrata = window.document.querySelector('div#Caprese')
+    Burrata.innerHTML = `<p class="infor">&#x02022;Molho de tomates frescos;<br>&#x02022;Muçarela de búfala;<br>&#x02022;Rodelas de tomate;<br>&#x02022;Pesto de azeitonas pretas e<br>&#x02022;Folhas de manjericão.</p>`
+}
+function Genovese() {
+    var Genovese  = window.document.querySelector('div#Genovese')
+    Genovese.innerHTML = `<p class="infor">&#x02022;Molho de tomates frescos;<br>&#x02022;Muçarela de Búfala e<br>&#x02022;Pesto de manjericão.</p>`
+}
+function Romana() {
+    var Romana = window.document.querySelector('div#Romana')
+    Romana.innerHTML = `<p class="infor">&#x02022;Molho de tomates frescos;<br>&#x02022;Muçarela;<br>&#x02022;Gorgonzola;<br>&#x02022;Catupiry original e<br>&#x02022;Parmesão.</p>`
+}
+function Royale() {
+    var Royale = window.document.querySelector('div#Royale')
+    Royale.innerHTML = `<p class="infor">&#x02022;Molho de Tomates frescos;<br>&#x02022;Muçarela;<br>&#x02022;Presunto royale;<br>&#x02022;Queijo brie  e<br>&#x02022;Geléia de Morango.</p>`
+}
+function Capri() {
+    var Capri = window.document.querySelector('div#Capri')
+    Capri.innerHTML = `<p class="infor">&#x02022;Molho de Tomates frescos;<br>&#x02022;Muçarela;<br>&#x02022;Carne de Sol desfiada;<br>&#x02022;Queijo coalho  e<br>&#x02022;Catupiry original.</p>`
+}
+function Toscana() {
+    var Toscana = window.document.querySelector('div#Toscana')
+    Toscana.innerHTML = `<p class="infor">&#x02022;Molho de Tomates frescos;<br>&#x02022;Calabresa defumada  e<br>&#x02022;Cebola roxa.</p>`
+}
+function Julieta() {
+    var Julieta = window.document.querySelector('div#Julieta')
+    Julieta.innerHTML = `<p class="infor">&#x02022;Goiabada Cascão;<br>&#x02022;Queijo Canastra  e<br>&#x02022;Crumble de castanha.</p>`
+}
+
+function Cartola() {
+    var Cartola = window.document.querySelector('div#Cartola')
+    Cartola.innerHTML = `<p class="infor">&#x02022;Banana caramelada;<br>&#x02022;Queijo muçarela;<br>&#x02022;Finalizada com canela  e açúcar.</p>`
+}
+function Veneza() {
+    var Veneza = window.document.querySelector('div#Veneza')
+    Veneza.innerHTML = `<p class="infor">&#x02022;Uva verde;<br>&#x02022;Queijo muçarela;<br>&#x02022;Ganache  e<br>&#x02022;Chocolate.</p>`
+}
+/*function () {
+    var  = window.document.querySelector('div#')
+    .innerHTML = `<p class="infor">&#x02022;;
+    <br>&#x02022;;
+    <br>&#x02022;;
+    <br>&#x02022;;
+    <br>&#x02022;  e
+    <br>&#x02022;.</p>`
+}*/
